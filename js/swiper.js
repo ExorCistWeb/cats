f5ef61fb80bfe75cfde46939e0a97b19cc9a9440
ExorCistWeb <75250883+ExorCistWeb@users.noreply.github.com>
2024-11-23 23:55:07 +0700

    Add files via upload

---
+++ js/swiper.js
@@ -0,0 +1,8 @@
+var swiper = new Swiper(".availableSwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
